30f39ad814e877aaf99db67a8d3adaf94b9d938f
Sam Chan <spc28@duke.edu>
2020-04-07 23:34:32 -0400

    merge storyboard with backend, create singleton instance of firebase

--- whereyouat/whereyouat/ViewController.swift
+++ whereyouat/whereyouat/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var api = API.sharedInstance
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
