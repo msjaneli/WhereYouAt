00e908485ebcf3ccb6aad2c8d00a2a5147331c32
aa485 <aa485@duke.edu>
2020-04-11 16:28:41 -0500

    fixed error with NSUserDefaults

--- whereyouat/whereyouat/AddFriendsViewController.swift
+++ whereyouat/whereyouat/AddFriendsViewController.swift
@@ -32,9 +32,9 @@ class AddFriendsViewController: UIViewController, UITextFieldDelegate {
     }
     //when clicking on add friends image on second screen, call addUserFriends
     var api = API.sharedInstance
-    let userDefault = UserDefaults.standard
+    let myUsername = UserDefaults.standard.string(forKey: "username") ?? nil
     @IBAction func onSecondButtonClicked(_ sender: Any) {
-        api.addUserFriends(username1: userDefault.text!, username2: friendUsername.text!)
+        api.addUserFriends(username1: myUsername!, username2: friendUsername.text!)
     }
     // @objc func buttonClick()
     // api.addUserFriends(username1: username, username2: friendUsername)
